c86691088063a078d33344ef410ff8a09b736d1e
luis <luisgonzalezyante@gmail.com>
2020-03-17 22:02:01 -0400

    [wip] able to use get endpoint

--- services/AeroQualService.js
+++ services/AeroQualService.js
@@ -3,13 +3,14 @@ const axios = require('axios');
 
 const BASE_URL = "http://cloud.aeroqual.com";
 const LOGIN_URL = `${BASE_URL}/api/account/login`;
-const DATA_URL = `${BASE_URL}/api/data/serial`;
+const INTRUSTMENT_URL = `${BASE_URL}/api/instrument`;
+const DATA_URL = `${BASE_URL}/api/data/`;
 
 const OBSERVATION_URL = `${BASE_URL}/aq/observation/zipCode/current`;
 let AUTH_TOKEN = null;
 // TODO Do Not Commit This
 const USERNAME = "sean@graceinactiondetroit.org";
-const PASSWORD = "Grac3'NAct10N";
+const PASSWORD = ;
 
 const login = async () => {
     return await axios.post(`${LOGIN_URL}`, {UserName: USERNAME, Password: PASSWORD});
@@ -19,8 +20,20 @@ const setAuthToken = async (data) => {
     AUTH_TOKEN = data.headers['set-cookie'][0];
 }
 
-const get = async (zipCode) => {
-    return await axios.get(`${OBSERVATION_URL}?format=application/json&zipCode=${zipCode}&distance=25&API_KEY=${API_KEY}`);
+const getInstruments = async () => {
+    return await axios.get(INTRUSTMENT_URL, {
+        headers: {
+            Cookie: AUTH_TOKEN
+        }
+    });
+}
+
+const get = async (instrument, from, to, averagingperiod, includejournal) => {
+    return await axios.get(`${DATA_URL}/${instrument}?from=${from}&to=${to}&averagingperiod=${averagingperiod}&includejournal=${includejournal}`, {
+        headers: {
+            Cookie: AUTH_TOKEN
+        }
+    });
 }
 
 const poll = async () => {
@@ -28,6 +41,25 @@ const poll = async () => {
         const res = await login();
         setAuthToken(res);
         console.log(AUTH_TOKEN);
+        const {data} = await getInstruments();
+        console.log(data);
+        const date = new Date();
+        date.setHours(0);
+        date.setMilliseconds(0);
+        date.setMinutes(0);
+        date.setSeconds(0);
+        const d = new Date();
+        d.setHours(0);
+        d.setMilliseconds(0);
+        d.setMinutes(0);
+        d.setSeconds(0);
+        date.setDate(date.getDate() -1);
+        const from = date.toISOString();
+        const to = d.toISOString();
+        const averagingperiod = 60;
+        const includejournal = false;
+        const a = await get(data[0], from.substring(0, from.length-5), to.substring(0, to.length-5), averagingperiod, includejournal);
+        console.log(a.data);
         // AirNowModel.create(data);
     } catch(err) {
         console.log('Failed to retrieve data', err);
