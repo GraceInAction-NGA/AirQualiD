428302da012c4f68bb8a110916451a5d067d13e5
luis <luisgonzalezyante@gmail.com>
2021-10-29 01:30:26 -0400

    renamed of model

--- models/AeroQualModel.js
+++ models/AeroQualModel.js
@@ -4,6 +4,7 @@ const AqiAdapter = require(process.cwd() + '/helpers/AqiAdapter');
 
 const create = async (data) => {
     // await firebase.database.collection("aeroqual").add(data);
+    
     const aqi = AqiAdapter.fromAeroQual(data);
     // await firebase.database.collection("aqis").add(aqi);
 }
